cfdf051c5c2ec1a1ed3a699d951b024a3c525062
Fedar Trukhan <alimath.work@gmail.com>
2015-03-01 01:40:12 +0300

    Initial Commit

---
+++ voziby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voziby
+//
+//  Created by Fedar Trukhan on 01.03.15.
+//  Copyright (c) 2015 Novum Studium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
